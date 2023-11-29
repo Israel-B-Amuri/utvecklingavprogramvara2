6f415ef56af9e41b7d5343a78bce829c1cac7a3f
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-11-29 08:44:55 +0200

    test

--- portfolio/src/Components/Projects.tsx
+++ portfolio/src/Components/Projects.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <div className='text-slate-50 h-40 w-full'>
         <div className='h-36 w-full'></div>
-        <div></div>
+        <div>hej</div>
         <div></div>
         <div></div>
     </div>
